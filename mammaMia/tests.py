ec7de560318448bfcca653f848b4ca70d00cde48
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-11-02 12:08:34 +0100

    Mi cambio

--- mammaMia/tests.py
+++ mammaMia/tests.py
@@ -2,3 +2,4 @@ from django.test import TestCase
 
 # Create your tests here.
 # Prueba de comentario para verificar el funcionamiento del sistema de control de versiones.
+# Prueba de comentario para verificar el funcionamiento del sistema de control de versiones (joni).
