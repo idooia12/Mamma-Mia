45bb9c12ea989ea45bd9c9cd715e6857ff757fc4
Irene Montserrat <irene.montserrat@opendeusto.es>
2025-11-04 12:51:31 +0100

    Config views e index.html

--- proyectoWeb/mammaMia/views.py
+++ proyectoWeb/mammaMia/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import Masa, Ingrediente, Pizza
 # Create your views here.
+
+from django.shortcuts import render
+from .models import Pizza
+
+def index(request):
+    pizzas = Pizza.objects.all()  # Consulta todas las pizzas
+    return render(request, 'mammaMia/index.html', {'pizzas': pizzas})
