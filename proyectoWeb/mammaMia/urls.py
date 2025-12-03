34e549203630f757fafa89c13fc73475ce434b47
Idoia Bergera <idoia.bergera@opendeusto.es>
2025-12-03 09:14:54 +0100

    vistas basadas en clases O2

--- proyectoWeb/mammaMia/urls.py
+++ proyectoWeb/mammaMia/urls.py
@@ -1,12 +1,15 @@
+#urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('pizza/<int:pk>/', views.DetallePizza.as_view(), name='detallePizza'),
-    path('pizzas/', views.todasLasPizzas, name='todasLasPizzas'),
-     path('masas/', views.todasLasMasas, name='todasLasMasas'),
+    path('', views.IndexView.as_view(), name='index'),
+    
+    path('pizzas/', views.ListaPizzas.as_view(), name='todas_pizzas'),
+    path('masas/', views.ListaMasas.as_view(), name='todas_masas'),
+    path('ingredientes/', views.ListaIngredientes.as_view(), name='todos_ingredientes'),
+    
     path('masa/<int:pk>/', views.DetalleMasa.as_view(), name='detalleMasa'),
-    path('ingredientes/', views.todosLosIngredientes, name='todosIngredientes'),
     path('ingrediente/<int:pk>/', views.DetalleIngrediente.as_view(), name='detalleIngrediente'),
+    path('pizza/<int:pk>/', views.DetallePizza.as_view(), name='detallePizza'),
 ]
--- proyectoWeb/mammaMia/views.py
+++ proyectoWeb/mammaMia/views.py
@@ -1,11 +1,11 @@
+#mammaMia/views.py
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView
 from .models import Masa, Ingrediente, Pizza
-# Create your views here.
 
-from django.shortcuts import render
-from .models import Pizza
+#VISTAS BASADAS EN CLASES (OPCION 2)
 
+'''
 def index(request):
     masas = Masa.objects.all()
     pizzas_destacadas = []
@@ -15,47 +15,86 @@ def index(request):
             pizzas_destacadas.append(pizza)
 
     return render(request, 'mammaMia/index.html', {'pizzas': pizzas_destacadas})
+'''
 
-def todasLasMasas(request):
-    masas = Masa.objects.all().order_by('nombre')
-    return render(request, 'mammaMia/todasMasas.html', {'masas': masas})
+class IndexView(TemplateView):
+    template_name = 'mammaMia/index.html'
 
-def todosLosIngredientes(request):
-    ingredientes = Ingrediente.objects.all().order_by('nombre')
-    return render(request, 'mammaMia/todosIngredientes.html', {'ingredientes': ingredientes})
+    def get_context_data(self, **kwargs):
+        # Recuperamos el contexto base
+        context = super().get_context_data(**kwargs)
+        
+        # Lógica original: buscar la pizza más barata de cada masa
+        masas = Masa.objects.all()
+        pizzas_destacadas = []
+        for masa in masas:
+            pizza = Pizza.objects.filter(masa=masa).order_by('precio').first()
+            if pizza:
+                pizzas_destacadas.append(pizza)
+        
+        # Añadimos la variable al contexto
+        context['pizzas'] = pizzas_destacadas
+        return context
 
+class ListaMasas(ListView):
+    model = Masa
+    template_name = 'mammaMia/todasMasas.html'
+    context_object_name = 'masas'
+    ordering = ['nombre'] # Ordenar por nombre
 
-def todasLasPizzas(request):
-    pizzas = Pizza.objects.all().order_by('precio')
-    masas = Masa.objects.all().order_by('nombre')
-    ingredientes = Ingrediente.objects.all().order_by('nombre')
-    
-    # Obtenemos los filtros enviados desde el formulario
-    masa_id = request.GET.get('masa')
-    ingredientes_ids = request.GET.getlist('ingredientes')
+class ListaIngredientes(ListView):
+    model = Ingrediente
+    template_name = 'mammaMia/todosIngredientes.html'
+    context_object_name = 'ingredientes'
+    ordering = ['nombre']
 
-    # Filtro por masa
-    if masa_id:
-        pizzas = pizzas.filter(masa_id=masa_id)
+class ListaPizzas(ListView):
+    model = Pizza
+    template_name = 'mammaMia/todasPizzas.html'
+    context_object_name = 'pizzas'
+    ordering = ['precio'] # Orden por defecto
 
-    # Filtro por ingredientes (todas las seleccionadas deben estar en la pizza)
-    if ingredientes_ids:
-        for ing_id in ingredientes_ids:
-            pizzas = pizzas.filter(ingredientes__id=ing_id)
+    #Sobreescribir este metodo para aplicar los filtros
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        
+        # Filtros de la URL (GET)
+        masa_id = self.request.GET.get('masa')
+        ingredientes_ids = self.request.GET.getlist('ingredientes')
 
-    pizzas = pizzas.distinct()  # evitar duplicados si hay joins
+        # Filtro por masa
+        if masa_id:
+            queryset = queryset.filter(masa_id=masa_id)
 
-    context = {
-        'pizzas': pizzas,
-        'masas': masas,
-        'ingredientes': ingredientes,
-        'masa_seleccionada': masa_id,
-        'ingredientes_seleccionados': [int(i) for i in ingredientes_ids],
-    }
+        # Filtro por ingredientes
+        if ingredientes_ids:
+            for ing_id in ingredientes_ids:
+                queryset = queryset.filter(ingredientes__id=ing_id)
 
-    return render(request, 'mammaMia/todasPizzas.html', context)
+        return queryset.distinct()
 
+    #Sobreescribir método para enviar datos EXTRA al template y que pueda pintar el formulario de filtros.
+    def get_context_data(self, **kwargs):
 
+        context = super().get_context_data(**kwargs)
+        
+        # Listas para rellenar los selects/checkbox del formulario
+        context['masas'] = Masa.objects.all().order_by('nombre')
+        context['ingredientes'] = Ingrediente.objects.all().order_by('nombre')
+        
+        # Mantener lo seleccionado para que no se borre al filtrar
+        context['masa_seleccionada'] = self.request.GET.get('masa')
+        ingredientes_ids = self.request.GET.getlist('ingredientes')
+        
+        # Convertimos a enteros para poder comparar en el template
+        if ingredientes_ids:
+            context['ingredientes_seleccionados'] = [int(i) for i in ingredientes_ids if i.isdigit()]
+        else:
+            context['ingredientes_seleccionados'] = []
+            
+        return context
+
+# VISTAS DETALLE
 class DetallePizza(DetailView):
     model = Pizza
     template_name = 'mammaMia/detallePizza.html'
